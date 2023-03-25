a6379ffbf586f5d62b99b624f7870283fb78ffec
Taylor Anderson <tganderson0@gmail.com>
2023-03-25 01:32:43 -0600

    added some bgm

--- Assets/CombatVisuals/ParticleHandler.cs
+++ Assets/CombatVisuals/ParticleHandler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class ParticleHandler : MonoBehaviour
 {
@@ -10,10 +11,27 @@ public class ParticleHandler : MonoBehaviour
     public ParticleSystem physCharging;
     public ParticleSystem physAttack;
 
+    public AudioSource charging;
+    public AudioSource attack;
+
+    public AudioClip magicChargingSoundEffect;
+    public AudioClip magicAttackSoundEffect;
+    public AudioClip physChargingSoundEffect;
+    public AudioClip physAttackSoundEffect;
+
 
     public void StartMagicCharge()
     {
         magicCharging.Play();
+    }
+
+    public void StartMagicChargingSoundEffect()
+    {
+        if (magicChargingSoundEffect != null)
+        {
+            charging.clip = magicChargingSoundEffect;
+            charging.Play();
+        }
     }
 
     public void StartMagicAttack()
@@ -21,14 +39,41 @@ public class ParticleHandler : MonoBehaviour
         magicAttack.Play();
     }
 
+    public void StartMagicAttackSoundEffect()
+    {
+        if (magicAttackSoundEffect != null)
+        {
+            attack.clip = magicAttackSoundEffect;
+            attack.Play();
+        }
+    }
+
     public void StartPhysCharging()
     {
         if (physCharging != null) physCharging.Play();
     }
 
+    public void StartPhysChargingSoundEffect()
+    {
+        if (physChargingSoundEffect != null)
+        {
+            charging.clip = physChargingSoundEffect;
+            charging.Play();
+        }
+    }
+
     public void StartPhysAttack()
     {
         physAttack.Play();
+    }
+
+    public void StartPhysAttackSoundEffect()
+    {
+        if (physAttackSoundEffect != null)
+        {
+            attack.clip = physAttackSoundEffect;
+            attack.Play();
+        }
     }
     
 }
